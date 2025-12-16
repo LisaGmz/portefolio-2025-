d86a007c6db9de1c37dcc1b6db70ef5e518cfce3
LisaGmz <lisa.gimenez99@gmail.com>
2025-12-16 15:08:20 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sert les fichiers du dossier "public"
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
